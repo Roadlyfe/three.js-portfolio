fcad2cf231f559eeb2e3a444c4bf612c17c5a67c
Adam Smith <adam@roadlyfe.com>
2023-01-03 13:25:46 -0800

    setting up renderer

--- main.js
+++ main.js
@@ -5,3 +5,13 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGL1Renderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( windwo.innerWidth, window.innerHeight );
+camera.position.setZ(30);
+
+renderer.render( scene, camera );
