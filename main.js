d43c8f49588acf7f7cdef53e4ae87f492357df7c
Adam Smith <adam@roadlyfe.com>
2023-01-06 11:43:24 -0800

    background image and random generated stars

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import './spaceweb.jpeg';
 
 // Setup
 
@@ -41,8 +42,20 @@ const controls = new OrbitControls(camera, renderer.domElement);
 function addStar() {
   const geometry = new THREE.SphereGeometry(0.25, 24, 24);
   const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
-  conststar = new THREEMesh( geometry, material );
+  const star = new THREE.Mesh( geometry, material );
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100 ));
+
+  star.position.set(x, y, z);
+  scene.add(star);
 }
+
+Array(200).fill().forEach(addStar);
+
+
+const spaceTexture = new THREE.TextureLoader().load('spaceweb.jpeg');
+scene.background = spaceTexture;
+// addStar();
 
 function animate() {
   requestAnimationFrame(animate);
